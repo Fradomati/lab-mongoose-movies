aa1ebbeee1a51ca7b635f48ee4a92ec1215c1cf5
Borja Domínguez <borja.d.atienza@gmail.com>
2020-02-02 13:16:22 +0100

    Movies Compass Generated

--- starter-code/seeds/seeds.js
+++ starter-code/seeds/seeds.js
@@ -2,6 +2,7 @@
 
 const withDbConnection = require("../withDbConnection");
 const celebrity = require("../models/celebrity");
+const movie = require("../models/Movie");
 
 withDbConnection(async () => {
   await celebrity.deleteMany();
@@ -9,5 +10,21 @@ withDbConnection(async () => {
     { name: "Manuel", ocuppation: "Actor", catchPhrase: "Ey, baby!" },
     { name: "Pedro", ocuppation: "Cantante", catchPhrase: "Mamma mía!" },
     { name: "Ivan", ocuppation: "Rockero", catchPhrase: "Welcome to hell" }
+  ]);
+
+  // Movies
+  await movie.deleteMany();
+  await movie.create([
+    { title: "Pinocho", genre: "Drama", plot: "Adolescencia" },
+    {
+      title: "Resacón",
+      genre: "Suspense",
+      plot: "Grupo de amigos que pierden a uno de sus hermanos"
+    },
+    {
+      title: "Torrente",
+      genre: "Acción",
+      plot: "Detective privado en busca de la justicia"
+    }
   ]);
 });
